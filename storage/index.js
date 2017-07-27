61370e358faeb2bab3293015774d2006838dfc97
clintcabanero <clint@bigleafmobile.com>
2017-07-26 20:10:16 -0700

    adds storage snippet

---
+++ storage/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const mountPoint = '/mnt/sda1'; // the first flash drive plugged in
+const filePath = path.join(mountPoint, 'testFile.txt');
+
+const textToWrite = 'Hello Tessel Storage';
+
+// write to storage
+fs.writeFile(filePath, textToWrite, function() {
+  console.log('Text file sent to ', filePath, 'on USB mass storage device.');
+});
+
+// read from storage
+fs.readFile(filePath, function (err, data) {
+  console.log('Read ', data.toString(), ' from USB mass storage device.');
+});
